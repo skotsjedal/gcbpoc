8ac1d6365582835153cdd335aed1b613815e1bc1
Andreas Skomedal <andreas.berg.skomedal@visma.com>
2016-04-23 13:22:08 +0200

    fixedrate

--- NewBehaviourScript.cs
+++ NewBehaviourScript.cs
@@ -5,20 +5,27 @@ using System;
 public class NewBehaviourScript : MonoBehaviour
 {
     Camera _cam;
+    float _speed = 0.01f;
 
     // Use this for initialization
     void Start()
     {
         _cam = Camera.main;
+        InvokeRepeating("Move", 1, 1f / 100);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    void Move()
+    {
         transform.localPosition = new Vector3(
-            transform.localPosition.x + _cam.transform.forward.x * 0.001f,
-            transform.localPosition.y + _cam.transform.forward.y * 0.001f,
-            transform.localPosition.z + _cam.transform.forward.z * 0.001f
+            transform.localPosition.x + _cam.transform.forward.x * _speed,
+            transform.localPosition.y + _cam.transform.forward.y * _speed,
+            transform.localPosition.z + _cam.transform.forward.z * _speed
             );
     }
 }
